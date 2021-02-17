50f6c9db6e12d47401b95514476bdf28a733c9e8
Kristof <kristof@aut.bme.hu>
2021-02-17 16:42:21 +0100

    Initial commit

---
+++ ShoppingTests/Basics.cs
@@ -0,0 +1,20 @@
+using Shopping;
+using System;
+using Xunit;
+
+namespace ShoppingTests
+{
+    // Stryker.NET mutation testing tool:
+    //  https://github.com/stryker-mutator/stryker-net
+    // Moq package:
+    //  https://github.com/moq/moq4
+    public class Basics
+    {
+        [Fact]
+        public void Instantiation()
+        {
+            var pc = new PriceCalculator();
+            Assert.NotNull(pc);
+        }
+    }
+}
