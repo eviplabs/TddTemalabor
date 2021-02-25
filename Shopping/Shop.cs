9364d2d767ec8e68a63261a665a196c552468114
bvajk <71184810+bvajk@users.noreply.github.com>
2021-02-25 11:58:01 +0100

    ProductRegistration és GetPrice

--- Shopping/Shop.cs
+++ Shopping/Shop.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Shopping
 {
-    public class PriceCalculator
+    public class Shop
     {
+        private Dictionary<string, int> products = new Dictionary<string, int>();
+        public void RegisterProduct(string name, int price)
+        {
+            products.Add(name, price);
+        }
+        public int GetPrice(string name)
+        {
+            return products[name];
+        }
     }
 }
