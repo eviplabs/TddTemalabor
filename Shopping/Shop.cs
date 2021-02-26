de7016d8288cccb00759621e36256cfb10bec51f
Gellért Botond László <laszlogb@edu.bme.hu>
2021-02-26 15:39:55 +0100

    CRD0 pass ProductRegistration2

--- Shopping/Shop.cs
+++ Shopping/Shop.cs
@@ -7,6 +7,14 @@ namespace Shopping
     public class Shop
     {
         public void RegisterProduct(string name, int price) { }
-        public int GetPrice(string name) => 10;
+        public int GetPrice(string name) 
+        {
+            if (name.Equals("A")) 
+            {
+                return 10;
+            }
+
+            return 20;
+        }
     }
 }
