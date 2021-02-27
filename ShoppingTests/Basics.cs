fd5b4ce45161404ba04436cf5ad17ca42b02a31c
Zsolt Demeter <demeter52@windowslive.com>
2021-02-27 16:08:57 +0100

    Alapfeladat: 2 piros teszt

--- ShoppingTests/Basics.cs
+++ ShoppingTests/Basics.cs
@@ -22,9 +22,21 @@ namespace ShoppingTests
         {
             var s = new Shop();
             s.RegisterProduct("A", 10); //szerintem ez véletlen volt char
+            s.RegisterProduct("A", 1000);
             s.RegisterProduct("C", 20);
             s.RegisterProduct("E", 50);
             var price = s.GetPrice("AACEE");
+            Assert.Equal(140, price);
+        }
+
+        [Fact]
+        public void NotRegisteredProductInGetProce()
+        {
+            var s = new Shop();
+            s.RegisterProduct("A", 10);
+            s.RegisterProduct("C", 20);
+            s.RegisterProduct("E", 50);
+            var price = s.GetPrice("AACEEZ");
             Assert.Equal(140, price);
         }
 
