b2206e3f38423213abed81b80a880f9d9af916c6
Zsolt Demeter <demeter52@windowslive.com>
2021-02-27 15:57:18 +0100

    CRD P01 AmountDiscount teszt kizolditve

--- Shopping/Shop.cs
+++ Shopping/Shop.cs
@@ -14,17 +14,40 @@ namespace Shopping
         public int GetPrice(string name)
         {
             char[] elements = name.ToCharArray();
-            int price = 0;
-            foreach (var item in elements)
+            // Megszamoljuk, hogy az egyes termekek hanyszor szereplnek
+            Dictionary<string, int> productCounts = new Dictionary<string, int>();
+            foreach (char item in elements)
             {
-                price += products[item.ToString()];
+                if (!productCounts.ContainsKey(item.ToString()))
+                {
+                    productCounts.Add(item.ToString(), 1);
+                }
+                else
+                {
+                    productCounts[item.ToString()]++;
+                }
+            }
+
+            /* osszeadjuk a termekek arat a darabszamukat-, es az erre vonatkozo
+            esetleges kedvezmenyeket figyelembe veve */
+            int price = 0;
+            foreach ((string product, int count) in productCounts)
+            {
+                if (product == "A" && count >= 5)
+                {
+                    price += (int)(products["A"] * count * 0.9);
+                }
+                else
+                {
+                    price += products[product] * count;
+                }
             }
             return price;
         }
 
         public void RegisterAmountDiscount(string v1, int v2, double v3)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
         }
     }
 }
