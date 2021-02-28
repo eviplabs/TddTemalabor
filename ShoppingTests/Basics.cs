18509d364a7097c31766f5ea6cc1b221e36f85c6
Olajos Martina <Martina.Olajos@loxon.eu>
2021-02-28 19:33:42 +0100

    CRD P01 - Új piros teszt a registerAmountDiscount metódus implementálásához

--- ShoppingTests/Basics.cs
+++ ShoppingTests/Basics.cs
@@ -45,9 +45,20 @@ namespace ShoppingTests
             var s = new Shop();
             s.RegisterProduct("A", 10);
             s.RegisterProduct("B", 100);
-            s.RegisterAmountDiscount("A", 5, 0.9);  //Piros
+            s.RegisterAmountDiscount("A", 5, 0.9); 
             var price = s.GetPrice("AAAAAAB");
             Assert.Equal(154, price);
+        }
+
+        [Fact]
+        public void AmountWithRegisteredDiscount()
+        {
+            var s = new Shop();
+            s.RegisterProduct("A", 10);
+            s.RegisterProduct("B", 100);
+            s.RegisterAmountDiscount("A", 3, 0.7);  //Piros - jelenleg az 5 darab és 10% kedvezmény beégetett, a RegisterAmountDiscount értéke nincs használva
+            var price = s.GetPrice("AAAABB");
+            Assert.Equal(228, price);
         }
     }
 }
