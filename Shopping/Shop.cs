cd1aa6eaf0b63ed99a34aeec39df06e74e16791f
András Antoni <starchmenrt@gmail.com>
2021-03-01 13:58:12 +0100

    CRD0 pass PriceSum3

--- Shopping/Shop.cs
+++ Shopping/Shop.cs
@@ -6,19 +6,24 @@ namespace Shopping
 {
     public class Shop
     {
-        public void RegisterProduct(string name, int price) { }
+        private Dictionary<string, int> Products;
+        public Shop()
+        {
+            Products= new Dictionary<string, int>();
+        }
+        public void RegisterProduct(string name, int price) 
+        {
+            Products[name] = price;
+        }
+
         public int GetPrice(string name) 
         {
-            switch (name)
+            int price = 0;
+            foreach (var item in name)
             {
-                case "A":return 10;
-                case "B":return 20;
-                case "C":return 40;
-                case "ABC": return 60;
-                case "ABCDA": return 110;
-                default: return 0;
+                price += Products[item.ToString()];
             }
-
+            return price;
         }
     }
 }
