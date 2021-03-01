285c86d405b5a9c999351394606b13b31b8b3480
Hajnal Gábor Márk <hajnalgabormark@edu.bme.hu>
2021-03-01 14:43:51 +0100

    CRD1 RegisterAmountDiscount2 passed

--- Shopping/Shop.cs
+++ Shopping/Shop.cs
@@ -7,29 +7,61 @@ namespace Shopping
     public class Shop
     {
         private Dictionary<string, int> Products;
+        private Dictionary<string, (int, double)> Discounts;
         public Shop()
         {
             Products= new Dictionary<string, int>();
+            Discounts = new Dictionary<string, (int, double)>();
         }
         public void RegisterProduct(string name, int price) 
         {
             Products[name] = price;
         }
 
-        public int GetPrice(string name) 
+        public double GetPrice(string name) 
         {
-            if (name.Equals("AAAAAAB")) return 154;
-            int price = 0;
-            foreach (var item in name)
+            double price = 0;
+
+            Dictionary<char, int> ProductCount = new Dictionary<char, int>();
+
+            foreach(char c in name)
             {
-                price += Products[item.ToString()];
+                if (ProductCount.ContainsKey(c))
+                {
+                    ProductCount[c]++;
+                }
+                else
+                {
+                    ProductCount[c] = 1;
+                }
             }
+
+
+            foreach(var key in ProductCount.Keys)
+            {
+                if (Discounts.ContainsKey(key.ToString()))
+                {
+                    if (ProductCount[key] >= Discounts[key.ToString()].Item1)
+                    {
+                        price += ProductCount[key] * Discounts[key.ToString()].Item2 * Products[key.ToString()];
+                    }
+                    else
+                    {
+                        price += ProductCount[key] * Products[key.ToString()];
+                    }
+                }
+                else
+                {
+                    price+=ProductCount[key] * Products[key.ToString()];
+                }
+            }
+
             return price;
         }
 
         public void RegisterAmountDiscount(string name,int amount,double percent) 
         {
-
+            Discounts[name] = (amount, percent);
         }
     }
 }
