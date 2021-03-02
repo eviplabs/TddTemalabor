1e6440dfa5964c67a36584eab1d6898c4db56abc
bvajk <71184810+bvajk@users.noreply.github.com>
2021-03-02 11:58:13 +0100

    CRD P02 teszt kizöldítve, RegisterCountDiscount -> CountDiscount és ehhez az árszámítás implementálva

---
+++ Shopping/CountDiscount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping
+{
+    class CountDiscount
+    {
+        public string ProductName { get; set; }
+        public int Buy { get; set; } 
+        public int Get { get; set; } //Buy-t fizet Get-et vihet
+        public CountDiscount(string productName, int buy, int get)
+        {
+            this.ProductName = productName;
+            this.Buy = buy;
+            this.Get = get;
+        }
+    }
+}
--- Shopping/Shop.cs
+++ Shopping/Shop.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Shopping
@@ -8,6 +9,7 @@ namespace Shopping
     {
         private Dictionary<string, int> products = new Dictionary<string, int>();
         private List<AmountDiscount> amountDiscounts = new List<AmountDiscount>();
+        private List<CountDiscount> countDiscounts = new List<CountDiscount>();
         public void RegisterProduct(string name, int price)
         {
             products.Add(name, price);
@@ -56,6 +58,7 @@ namespace Shopping
                     }
                 }
             }
+            price = getUpdatedCountDiscountPrice(name, price);
             return price;
         }
 
@@ -64,9 +67,46 @@ namespace Shopping
             amountDiscounts.Add(new AmountDiscount(name,amount,factor));
         }
 
-        public void RegisterCountDiscount(string name, int amountToPay, int amountToGet)
+        public void RegisterCountDiscount(string name, int amountToBuy, int amountToGet)
         {
-            //A következő teszt zöldítéséhez implementálandó.
+            countDiscounts.Add(new CountDiscount(name, amountToBuy, amountToGet));
+        }
+        private int getUpdatedCountDiscountPrice(string cart, int price)
+        {
+            // Összeszedi a különböző elemeket, és azoknak a számát
+            Dictionary<string, int> itemsAndCounts = new Dictionary<string, int>();
+            foreach (char itemchar in cart)
+            {
+                string item = itemchar.ToString();
+                if (itemsAndCounts.ContainsKey(item))
+                {
+                    itemsAndCounts[item]++;
+                }
+                else
+                {
+                    itemsAndCounts.Add(item, 1);
+                }
+            }
+            // Az egyes termékeknek ha van CountDiscount akciója, akkor elosztja maradéktalanul a termékek
+            // számát a m-el (az n-t fizet m-t vihetből), és kivonja a termék árát az eredmény szorzatával az egészből
+            foreach(KeyValuePair<string,int> kvp in itemsAndCounts)
+            {
+                // Ha nincs ilyen akció átugorja az iterációt
+                if (countDiscounts.Where(r => r.ProductName == kvp.Key).Count() == 0)
+                {
+                    continue;
+                }
+                CountDiscount countDiscount = countDiscounts.Where(r => r.ProductName == kvp.Key).First<CountDiscount>();
+                // Ha van amountDiscount, akkor az is kell az új ár kiszámolásához
+                double actualItemPrice = products[kvp.Key];
+                if (amountDiscounts.Where(r => r.ProductName == kvp.Key).Count() != 0)
+                {
+                    actualItemPrice *= amountDiscounts.Where(r => r.ProductName == kvp.Key).First<AmountDiscount>().Factor;
+                }
+                price -= (int)(actualItemPrice / (double)(countDiscount.Get - countDiscount.Buy)); //példányosított egyet fizet egyet vihet akció esetén nullával osztás fordul elő
+            }
+            // Ez a megoldás nem veszi figyelembe, ha később lesz több amountdiscount/countdiscount ugyan azon a terméken
+            return price;
         }
     }
 }
