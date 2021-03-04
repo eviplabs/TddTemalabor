50923607bef9da3f2234f8b64831bb2bf976953b
Zsolt István Kovács <kovacs.zsolt.istvan@edu.bme.hu>
2021-03-04 11:27:43 +0100

    Teszt hozzáadva a RegisterProduct és a GetPrice meglétére

--- ShoppingTests/Basics.cs
+++ ShoppingTests/Basics.cs
@@ -17,5 +17,14 @@ namespace ShoppingTests
             var pc = new Shop();
             Assert.NotNull(pc);
         }
+        [Fact]
+        public void ProductRegistration()
+        {
+            var sh = new Shop();
+            sh.RegisterProduct('A', 10);
+            var price = sh.GetPrice("AAA");
+            //Ellenörzi, hogy a price objektum integer-e
+            Assert.IsType<int>(price);
+        }
     }   
 }
