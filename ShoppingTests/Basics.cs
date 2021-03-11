c7ac19db2aac19f70a408abd1b0acd7dd224abf3
Balázs Horváth <bhorvath22@edu.bme.hu>
2021-03-11 16:19:37 +0100

    CRD P06 test: AddPointsToSupershopCardMoreNumberInProductName added

--- ShoppingTests/Basics.cs
+++ ShoppingTests/Basics.cs
@@ -237,5 +237,19 @@ namespace ShoppingTests
             var points = Shop.GetSupershopPoints(price);
             Assert.Equal(1,points);
         }
+
+        [Fact]
+        public void AddPointsToSupershopCardMoreNumberInProductsName() 
+        {
+            Shop.RegisterProduct('A', 20);
+            Shop.RegisterProduct('B', 30);
+            Shop.RegisterProduct('C', 50);
+            Shop.RegisterProduct('D', 70);
+
+            var price = Shop.GetPrice("AB2C3D1p");
+            var points = Shop.GetSupershopPoints(price);
+            //20+30+50+70 = 170 * 0.01 = 1.7
+            Assert.Equal(1.73, points);
+        }
     }
 }
