3408851147b1c58a32255d71d14cfb51aa83803d
Balázs Horváth <bhorvath22@edu.bme.hu>
2021-03-11 16:06:46 +0100

    CRD P06 tests refactored.

--- ShoppingTests/Basics.cs
+++ ShoppingTests/Basics.cs
@@ -10,80 +10,73 @@ namespace ShoppingTests
     //  https://github.com/moq/moq4
     public class Basics
     {
+        private readonly Shop Shop = new Shop();
         [Fact]
         public void Instantiation()
         {
-            var s = new Shop();
-            Assert.NotNull(s);
+            Assert.NotNull(Shop);
         }
 
         [Fact]
         public void ProductRegistration()
         {
-            var s = new Shop();
-            s.RegisterProduct('A', 10);
-            var price = s.GetPrice("A");
+            Shop.RegisterProduct('A', 10);
+            var price = Shop.GetPrice("A");
             Assert.Equal(10, price);
         }
 
         [Fact]
         public void ProductRegistration2()
         {
-            var s = new Shop();
-            s.RegisterProduct('B', 20);
-            var price = s.GetPrice("B");
+            Shop.RegisterProduct('B', 20);
+            var price = Shop.GetPrice("B");
             Assert.Equal(20, price);
         }
 
         [Fact]
         public void ProductRegistration3()
         {
-            var s = new Shop();
-            s.RegisterProduct('C', 40);
-            var price = s.GetPrice("C");
+            Shop.RegisterProduct('C', 40);
+            var price = Shop.GetPrice("C");
             Assert.Equal(40, price);
         }
 
         [Fact]
         public void PriceSum()
         {
-            var s = new Shop();
-            s.RegisterProduct('A', 10);
-            s.RegisterProduct('B', 20);
-            s.RegisterProduct('C', 30);
-            var price = s.GetPrice("ABC");
+            Shop.RegisterProduct('A', 10);
+            Shop.RegisterProduct('B', 20);
+            Shop.RegisterProduct('C', 30);
+            var price = Shop.GetPrice("ABC");
             Assert.Equal(60, price);
         }
 
         [Fact]
         public void PriceSum2()
         {
-            var s = new Shop();
-            s.RegisterProduct('A', 10);
-            s.RegisterProduct('B', 20);
-            s.RegisterProduct('C', 30);
-            s.RegisterProduct('D', 40);
-            var price = s.GetPrice("ABCDA");
+            Shop.RegisterProduct('A', 10);
+            Shop.RegisterProduct('B', 20);
+            Shop.RegisterProduct('C', 30);
+            Shop.RegisterProduct('D', 40);
+            var price = Shop.GetPrice("ABCDA");
             Assert.Equal(110, price);
         }
 
         [Fact]
         public void PriceSum3() 
         {
-            var s = new Shop();
-            s.RegisterProduct('A', 10);
-            s.RegisterProduct('B', 20);
-            s.RegisterProduct('C', 30);
-            s.RegisterProduct('D', 40);
-            s.RegisterProduct('E', 50);
-            var price = s.GetPrice("ABCDE");
+            Shop.RegisterProduct('A', 10);
+            Shop.RegisterProduct('B', 20);
+            Shop.RegisterProduct('C', 30);
+            Shop.RegisterProduct('D', 40);
+            Shop.RegisterProduct('E', 50);
+            var price = Shop.GetPrice("ABCDE");
             Assert.Equal(150, price);
         }
 
         [Fact]
         public void RegisterAmountDiscount1()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 100);
             Shop.RegisterAmountDiscount('A', 5, 0.9);
@@ -94,7 +87,6 @@ namespace ShoppingTests
         [Fact]
         public void RegisterAmountDiscount2()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 100);
             Shop.RegisterAmountDiscount('A', 5, 0.9);
@@ -106,7 +98,6 @@ namespace ShoppingTests
         [Fact]
         public void RegisterCountDiscount1()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('E', 50);
             Shop.RegisterCountDiscount('A', 3, 4);
@@ -119,7 +110,6 @@ namespace ShoppingTests
         [Fact]
         public void RegisterCountDiscount2()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 20);
             Shop.RegisterProduct('C', 30);
@@ -133,7 +123,6 @@ namespace ShoppingTests
         [Fact]
         public void RegisterCountDiscount3()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 20);
             Shop.RegisterProduct('C', 30);
@@ -146,7 +135,6 @@ namespace ShoppingTests
         [Fact]
         public void RegisterComboDiscount1()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 20);
             Shop.RegisterProduct('C', 50);
@@ -160,7 +148,6 @@ namespace ShoppingTests
         [Fact]
         public void RegisterComboDiscount2()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 20);
             Shop.RegisterProduct('C', 50);
@@ -175,7 +162,6 @@ namespace ShoppingTests
         [Fact]
         public void RegisterComboDiscount3()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 20);
             Shop.RegisterProduct('C', 50);
@@ -189,7 +175,6 @@ namespace ShoppingTests
         [Fact]
         public void RegisterClubMembership()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 20);
             Shop.RegisterProduct('C', 50);
@@ -202,7 +187,6 @@ namespace ShoppingTests
         [Fact]
         public void When_MoretInGetPrice_Expect_IgnoreThemExceptOne()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 20);
             Shop.RegisterProduct('C', 50);
@@ -215,7 +199,6 @@ namespace ShoppingTests
         [Fact]
         public void Register_Club_Membership_With_Different_Prices()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 10);
             Shop.RegisterProduct('B', 20);
             Shop.RegisterProduct('C', 30);
@@ -228,7 +211,6 @@ namespace ShoppingTests
         [Fact]
         public void Register_Club_Membership_Exclusive_Combo_Discount()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 20);
             Shop.RegisterProduct('B', 20);
             Shop.RegisterProduct('C', 30);
@@ -247,7 +229,6 @@ namespace ShoppingTests
         [Fact]
         public void AddPointsToSupershopCard()
         {
-            var Shop = new Shop();
             Shop.RegisterProduct('A', 20);
             Shop.RegisterProduct('B', 30);
             Shop.RegisterProduct('C', 50);
