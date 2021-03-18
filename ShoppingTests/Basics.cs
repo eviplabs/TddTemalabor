62ae21dcbc0969cdf28e26aa666d634873793532
Balázs Horváth <bhorvath22@edu.bme.hu>
2021-03-18 22:47:16 +0100

    CRD P06 test: PayWithSupershopPoints added

--- ShoppingTests/Basics.cs
+++ ShoppingTests/Basics.cs
@@ -251,5 +251,18 @@ namespace ShoppingTests
             //20+30+50+70 = 170 * 0.01 = 1.7
             Assert.Equal(1.7, points);
         }
+
+        [Fact]
+        public void PayWithSupershopPoints()
+        {
+            Shop.RegisterProduct('A', 20);
+            Shop.RegisterProduct('B', 30);
+            Shop.RegisterProduct('C', 50);
+
+            var price = Shop.GetPrice("ABC1p");
+            var supershopPoints = Shop.GetSupershopPoints(300);
+            //20+30+50 - 3 = 97
+            Assert.Equal(97, price - supershopPoints);
+        }
     }
 }
