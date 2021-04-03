df9e201713c296ed240bda8977366519159ba463
Balázs Horváth <bhorvath22@edu.bme.hu>
2021-04-03 20:22:51 +0200

    CRD P11: test RegisterUniqueCoupon added

---
+++ ShoppingTests/UniqueCouponTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shopping;
+using Xunit;
+
+namespace ShoppingTests
+{
+    public class UniqueCouponTests
+    {
+        public readonly Shop Shop = new Shop();
+        [Fact]
+        public void RegisterUniqueCoupon() 
+        {
+            Shop.RegisterProduct('A', 10);
+            Shop.RegisterProduct('B', 20);
+            Shop.RegisterCoupon("112554", 0.9);
+            //-10% kupon
+
+            var price = Shop.GetPrice("AABk112554");
+            //40*0,9 = 36
+            Assert.Equal(36, price);
+            
+            var price2 = Shop.GetPrice("AABk112554");
+            //40 mert a kupont már elhasználták
+            Assert.Equal(40, price2);
+        }
+    }
+}
