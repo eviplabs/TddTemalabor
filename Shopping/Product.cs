28ade243ec363782670157b5552e2ee163e4e938
Balázs Farkas <blaiseywolf@gmail.com>
2021-04-06 09:14:15 +0200

    Prodct class added.

---
+++ Shopping/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping
+{
+    class Product
+    {
+        public char name { get; private set; }
+        public int price { get; private set; }
+        public Product(char name, int price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
