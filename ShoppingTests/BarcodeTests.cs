f252f1af489eeccffb56683e528fbd352217744e
András Antoni <starchmenrt@gmail.com>
2021-04-09 10:56:17 +0200

    TEST: CRD P12: A2B2equalsAABB added

---
+++ ShoppingTests/BarcodeTests.cs
@@ -0,0 +1,23 @@
+﻿using Shopping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ShoppingTests
+{
+    public class BarcodeTests
+    {
+        public readonly Shop Shop = new Shop();
+
+        [Fact]
+        public void A2B2equalsAABB() {
+            Shop.RegisterProduct('A', 10);
+            Shop.RegisterProduct('B', 20);
+            var price = Shop.GetPrice("A2B2");
+            var price2 = Shop.GetPrice("AABB");
+            Assert.Equal(price, price2);
+
+        }
+    }
+}
