847a2c7e58f8fb87d5640f9a53787caa9ef23156
Hajnal Gábor Márk <hajnalgabormark@edu.bme.hu>
2021-04-12 23:30:29 +0200

    CRD D01 added: GetInventoryQuantity()

---
+++ ShoppingTests/InventoryTests.cs
@@ -0,0 +1,30 @@
+﻿using Shopping;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ShoppingTests
+{
+    public class InventoryTests
+    {
+        public readonly Shop Shop = new Shop();
+        public readonly IInventory Inventory = new Inventory();
+
+
+        [Fact]
+        public void GetInventoryQuantity()
+        {
+            Shop.RegisterProduct('A', 10);
+            Inventory.SetQuanity('A', 5);
+            Assert.Equal(5, Inventory.Products["A"]);
+            var price = Shop.GetPrice("A");
+            Assert.Equal(4, Inventory.Products["A"]);
+
+            Assert.Equal(10, price);
+            
+
+        }
+
+    }
+}
