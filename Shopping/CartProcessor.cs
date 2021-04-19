12a0ec61c01ff2c4edcc23c7ebb66d6fa2e580c3
Balázs Farkas <blaiseywolf@gmail.com>
2021-04-19 09:54:01 +0200

    CartProcessor created

---
+++ Shopping/CartProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shopping
+{
+    public static class CartProcessor
+    {
+        public static void processData()
+        {
+
+        }
+    }
+}
