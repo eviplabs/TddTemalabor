bb061ca9acf560878ca7792bcb60001412443cb0
Balázs Farkas <blaiseywolf@gmail.com>
2021-05-03 20:00:04 +0200

    AllDc Theory test added.

--- ShoppingTests/AllDcTests.cs
+++ ShoppingTests/AllDcTests.cs
@@ -7,30 +7,49 @@ namespace ShoppingTests
     public class AllDcTests : TestBase
     {
         #region Init
-        public AllDcTests() {}
+        public AllDcTests()
+        {
+            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], 4, 0.9));
+            sh.RegisterDiscount("ABC", new ComboDiscount(GetProductListABC(), 50));
+            sh.RegisterDiscount("C", new CountDiscount(sh.products['C'], 1, 2));
+        }
         #endregion
 
-        [Fact]
-        public void MultipleTypeDiscounts()
+        #region Helper Methods
+        private List<Product> GetProductListABC()
         {
             List<Product> productList = new List<Product>();
             productList.Add(sh.products['A']);
             productList.Add(sh.products['B']);
             productList.Add(sh.products['C']);
-            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], 4, 0.9));
-            sh.RegisterDiscount("ABC", new ComboDiscount(productList, 50));
-            sh.RegisterDiscount("C", new CountDiscount(sh.products['C'], 1, 2));
-            AssertPrice(186, "AAAAAAABBBCCC"); //280 - 90 - 4 - 0
+            return productList;
+        }
+        #endregion
+
+        #region Data
+        public static IEnumerable<object[]> GetDcCombinationData(int numTests)
+        {
+            var data = new List<object[]>
+            {
+                new object[] {100, "ABC3"},
+                new object[] {86, "A5BC"},
+                new object[] {86, "A4C2"},
+                new object[] {136, "A5BC3"},
+            };
+            return data;
+        }
+        #endregion
+
+        [Theory]
+        [MemberData(nameof(GetDcCombinationData), parameters: 2)]
+        public void MultipleTypeDiscounts(uint expected, string cart)
+        {
+            AssertPrice(expected, cart);
         }
 
         [Fact]
         public void MultipleTypeDiscountsTwoOfTheSameProduct()
         {
-            List<Product> productList = new List<Product>();
-            productList.Add(sh.products['A']);
-            productList.Add(sh.products['B']);
-            productList.Add(sh.products['C']);
-            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], 4, 0.9));
             Assert.Throws<System.ArgumentException>(delegate { sh.RegisterDiscount("A", new CountDiscount(sh.products['A'], 2, 4)); }); 
         }
     }
