db40f6e555037f8203cd9a1c2d05c020671686a0
Balázs Farkas <blaiseywolf@gmail.com>
2021-05-03 19:42:08 +0200

    AmountDc tests refactored

--- ShoppingTests/AmountDcTests.cs
+++ ShoppingTests/AmountDcTests.cs
@@ -1,5 +1,6 @@
 ﻿using Xunit;
 using Shopping;
+using System.Collections.Generic;
 
 namespace ShoppingTests
 {
@@ -16,6 +17,8 @@ namespace ShoppingTests
             sh.RegisterProduct('B', 20);
             sh.RegisterProduct('C', 50);
             sh.RegisterProduct('D', 100);
+
+            sh.RegisterDiscount("B", new AmountDiscount(sh.products['B'], 5, 0.9));
         }
         #endregion
 
@@ -27,55 +30,77 @@ namespace ShoppingTests
         }
         #endregion
 
-        [Fact]
-        public void RegisterAmountDiscount()
+        #region Data
+        public static IEnumerable<object[]> GetBasicAmountDcCalcData(int numTests)
         {
-            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], 5, 0.9));
-            AssertPrice(154, "AAAAAAD");
+            var data = new List<object[]>
+            {
+                new object[] {54, "A6", 4, 0.9},
+                new object[] {35, "A5", 5, 0.7},
+                new object[] {85, "A10", 8, 0.85},
+                new object[] {34, "A6", 3, 0.571},
+                new object[] {20, "A2" , 4, 0.9},
+                new object[] {224, "A6BCD", 4, 0.9},
+                new object[] {205, "A5BCD", 5, 0.7},
+                new object[] {255, "A10BCD", 8, 0.85},
+                new object[] {204, "A6BCD", 3, 0.571},
+                new object[] {190, "A2BCD" , 4, 0.9}
+            };
+            return data;
+        }
+        public static IEnumerable<object[]> GetMultipleDcData(int numTests)
+        {
+            var data = new List<object[]>
+            {
+                new object[] {315, "A10C5", (10U, 0.9), (5U, 0.9)},
+                new object[] {88, "A6B2", (5U, 0.8), (4U, 0.7)},
+                new object[] {80, "A5B2", (4U, 0.8), (3U, 0.7)},
+            };
+            return data;
+        }
+
+        #endregion
+
+        [Theory]
+        [MemberData(nameof(GetBasicAmountDcCalcData), parameters: 4)]
+        public void BasicAmountDcCalc(uint expected, string cart, uint amount, double multiplier)
+        {
+            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], amount, multiplier));
+            AssertPrice(expected, cart);
         }
 
         [Fact]
-        public void RegisterAmountDiscountWithFalseDiscount()
+        public void AmountDcWithInvalidData()
         {
-            sh.RegisterDiscount("B", new AmountDiscount(sh.products['B'], 5, 0.9));
-            AssertPrice(160, "AAAAAAD");
+            AssertPrice(160, "A6D");
         }
 
         [Fact]
-        public void RegisterAmountDiscountWithDifferentCharacters()
+        public void AmountDcWithDifferentProducts()
         {
             sh.RegisterProduct('E', 10);
             sh.RegisterProduct('G', 100);
             sh.RegisterDiscount("E", new AmountDiscount(sh.products['E'], 5, 0.9));
-            AssertPrice(154, "EEEEEEG");
+            AssertPrice(154, "E6G");
         }
-        [Fact]
-        public void ToggleDiscountOnlyForClubMembersAmount()
+        [Theory]
+        [InlineData(405, "D5v12", "12")]
+        [InlineData(500, "D5", "2")]
+        public void ToggleDcOnlyForSSMembers(uint expected, string cart, string userID)
         {
-            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], 5, 0.9, true));
-            sh.RegisterSuperShopCard("1");
-            AssertPrice(41, "AAAAAv1");
-            AssertPrice(50, "AAAAA");
-        }
-        [Fact]
-        public void RegisterAmountDiscountWithOnlyRequiquedProducts()
-        {
-            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], 5, 0.9));
-            AssertPrice(45, "A5");
-        }
-        [Fact]
-        public void RegisterAmountDiscountWithTwiceTheRequiquedProducts()
-        {
-            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], 5, 0.9));
-            AssertPrice(90, "A10");
+            sh.RegisterDiscount("D", new AmountDiscount(sh.products['D'], 5, 0.9, true));
+            sh.RegisterSuperShopCard(userID);
+            AssertPrice(expected, cart);
         }
 
-        [Fact]
-        public void RegisterAmountDiscountWithMultipleDiscounts()
+        [Theory]
+        [MemberData(nameof(GetMultipleDcData), parameters: 4)]
+        public void AmountDcWithMultipleDiscounts(uint expected, string cart,
+                      (uint req, double multiplier) pr1, (uint req, double multiplier) pr2)
         {
-            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], 5, 0.9));
-            sh.RegisterDiscount("B", new AmountDiscount(sh.products['B'], 5, 0.9));
-            AssertPrice(180, "A10B5");
+            sh.RegisterDiscount("A", new AmountDiscount(sh.products['A'], pr1.req, pr1.multiplier));
+            sh.RegisterDiscount("C", new AmountDiscount(sh.products['C'], pr2.req, pr2.multiplier));
+            AssertPrice(expected, cart);
         }
 
     }
